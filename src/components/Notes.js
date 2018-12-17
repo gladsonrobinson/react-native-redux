9712badb0f3279d0b2708118e8d77116653dfa8f
Gladson <grobinson@gulgnews.com>
2018-12-17 10:53:09 +0400

    data change

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,18 +3,13 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 import NoteItem from './NoteItem';
 
 export default class Notes extends Component {
-
-  _renderItem = ({item}) => {
-
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <FlatList data={this.props.notes}
           numColumns={2}
           horizontal={false}
-          renderItem={({item}) => <NoteItem note={item.key} />}/>
+          renderItem={({item}) => <NoteItem key={item.id} note={item.note} />}/>
       </View>
     );
   }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,8 @@ import {createStore} from 'redux';
 import AppReducer from './reducers';
 
 const initialState = { 
-    notes: [{key: 'x'},{key: 'b'},{key: 'a'}, {key: 'b'},{key: 'b'},{key: 'a'},{key: 'b'},{key: 'a'}, {key: 'b'},{key: 'b'},{key: 'a'},{key: 'b'},{key: 'a'}, {key: 'b'},{key: 'b'},{key: 'a'} ]
+    notes: [{id: 1, note: 'helloo'},{id: 2, note: 'dsfdsf'},{id: 3, note: 'fdsfjs'},{id: 4, note: 'vvsdfww'},{id: 5, note: 'nnweeef'},
+            {id: 6, note: 'helloo'},{id: 7, note: 'dsfdsf'},{id: 8, note: 'fdsfjs'},{id: 9, note: 'vvsdfww'},{id: 11, note: 'nnweeef'} ]
 };
 
 const store = createStore(AppReducer, initialState);
